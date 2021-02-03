8be5a7b320ad1af860dc1a178175f14aef2a2b73
Fabio Mazza <fab4mazz@gmail.com>
2021-02-03 12:22:01 +0100

    count valid times before loop

--- greedytracing.py
+++ greedytracing.py
@@ -6,13 +6,18 @@ import numba as nb
 
 from ranking import ranking_random, get_nonzero_idx_d
 
+@nb.njit(parallel=True)
 def _set_ranking(contacts_cut, contacts_c2, N, lamb, noise, maxS, minR):
     idxk = []
     #c2_i = contacts_c2[:,0]
+    #count_all = np.zeros(N, dtype=np.int)
     #c2_t = contacts_c2[:,2]
     scores = np.zeros(N)
     counts = np.zeros(N)
-    for i, j, t in contacts_cut:
+    for l in nb.prange(contacts_cut.shape[0]):
+        i = contacts_cut[l,0]
+        j = contacts_cut[l,1]
+        t = contacts_cut[l,2]
         # i to be estimated, j is infected
         if t > max(maxS[i], maxS[j]):
             if t < minR[j]:
@@ -23,11 +28,26 @@ def _set_ranking(contacts_cut, contacts_c2, N, lamb, noise, maxS, minR):
                 midx = (contacts_c2[:,0] == i) & (contacts_c2[:,2] > max(t, maxS[i]))
                 aux = contacts_c2[midx][:,1]
                 idxk.append(aux)
+                #count_all[aux] += 1
 
-    out_res = np.concatenate(idxk, axis=None)
-    return scores, counts, out_res
+    #out_res = np.concatenate(idxk, axis=None)
+    return scores, counts, idxk
 
+@nb.njit()
+def count_valid_c1(alli, allj, allt, maxS, minR):
+    """
+    Count valid contacts, given i, j, and t
+    """
+    res = np.zeros(alli.shape[0], dtype=np.bool_)
+    for l in nb.prange(alli.shape[0]):
+        i = alli[l]
+        j = allj[l]
+        t = allt[l]
+        if t > max(maxS[i], maxS[j]):
+            if t < minR[j]:
+                res[l] = True
 
+    return res
 
 def ranking_tracing_secnn(T, model, observations, params, noise = 1e-19):
     """
@@ -107,33 +127,49 @@ def ranking_tracing_secnn(T, model, observations, params, noise = 1e-19):
     #Score = dict([(i, 0) for i in range(model.N)])
     #Score = manager.dict([(i, 0) for i in range(model.N)])
     #Count = dict([(i, 0) for i in range(model.N)])
-    contacts_cut2 = dict()
-    if len(contacts_cut) > 0:
-        # (i,j) are both unknown
-        #print(datetime.datetime.now(), "make contacts_cut2")
-        #for i in idx_to_inf:
-        #    contacts_cut2[i] = contacts[(contacts["i"] == i) \
-        #                         & (contacts["j"].isin(idx_to_inf))]
-        contacts_cut2 = contacts[(contacts["i"].isin(idx_to_inf)) \
+    contacts_cut2 = contacts[(contacts["i"].isin(idx_to_inf)) \
                                 & (contacts["j"].isin(idx_to_inf))]
         #print(datetime.datetime.now(), "end contacts_cut2")
     print("t contacts cut: {:.3f} ms".format((time.time()-t0)*1000))
     t0 = time.time()
-    c_cut = contacts_cut[["i", "j", "t"]].to_numpy()
-    c_cut2=contacts_cut2[["i", "j", "t"]].to_numpy()
     #print(c_cut2)
-    Score, Count, idxk = _set_ranking(c_cut, c_cut2, model.N, lamb, noise, maxS, minR)
+    Score = np.zeros(model.N)
+    Count = np.zeros(model.N)
+    ##contacts_c1 = contacts_cut.to_numpy()
+    valid_c1 = count_valid_c1(*[contacts_cut[k].to_numpy() for k in ("i","j","t")], maxS, minR)
+    valid_contacts_c1 = contacts_cut.iloc[valid_c1]
+    t1 = time.time()-t0
+    idxk = []
+    for i, j, t in valid_contacts_c1[["i", "j", "t"]].to_numpy():
+        # i to be estimated, j is infected
+        if t > max(maxS[i], maxS[j]):
+            if t < minR[j]:
+                Score[i] += lamb + np.random.rand() * noise
+                Count[i] += 1.0
+                # get neighbors k from future contacts (i,k), from the set of the unknown nodes
+                #aux = contacts_cut2[i][(contacts_cut2["t"] > max(t, maxS[i]))]["j"].to_numpy()
+                aux = contacts_cut2[(contacts_cut2["i"] == i) & (contacts_cut2["t"] > max(t, maxS[i]) )]["j"].to_numpy()
+                idxk = np.concatenate((idxk, aux), axis = None).astype(np.int32)
+    t2 = time.time()-t0
+    t0 = time.time()
+    #idxk = np.concatenate(allidx, axis=None)
     #Score = pd.Series(Score)
     #print("t loop contacts: {:.3f} ms".format((time.time()-t0)*1000))
     #t0 = time.time()
     #np.save("idxk_new",idxk)
+    t3 = time.time() -t0
+    t0 = time.time()
     sec_NN = len(idxk)
     value_occ = Counter(idxk).items()
+    t4 = time.time() - t0
+    t0 = time.time()
     
     for (k, occk) in value_occ:
         Score[k] += lamb*lamb*occk 
-    
-    print("t contacts loop: {:.3f} ms".format((time.time()-t0)*1000))
+    tf = time.time() -t0
+    times = np.array(tuple((t1,t2,t3,t4,tf)))
+    print("t contacts loop: {:.3f} ms".format((times.sum())*1000))
+    print("times: ",times*1000)
     t0 = time.time()
 
     print(f"first NN c: {len(contacts_cut)}. second NN c: {sec_NN}")
